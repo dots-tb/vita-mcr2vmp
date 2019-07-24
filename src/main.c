dceea990045242c7ca9ca721d98893ccf0f12c00
AnalogMan151 <james.p.wolfe@outlook.com>
2019-07-24 12:09:15 -0400

    Updated Makefile and project folder structure.
    Plus some formatting fixes.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-//vita-mcr2vmp by @dots_tb - signs psx mcr files for use with the vita's psp emulator
+//vita-mcr2vmp by @dots_tb - signs PSOne MCR files to create VMP files for use with Sony Vita/PSP and exports MCR files from VMP
 //With help from the CBPS (https://discord.gg/2nDCbxJ) , especially:
-// Analog Man
+// @AnalogMan151
 // @teakhanirons
 
 #include <string.h>
@@ -8,8 +8,6 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <stdbool.h>
-
-#define CBC 1
 
 #include "aes.h"
 #include "sha1.h"
@@ -21,7 +19,7 @@ uint8_t iv[0x10] = {0xB3, 0x0F, 0xFE, 0xED, 0xB7, 0xDC, 0x5E, 0xB7, 0x13, 0x3D, 
 #define HASH_OFFSET 0x20
 #define MCR_OFFSET 0x80
 #define PMV_MAGIC 0x564D5000
-#define FILE_SZ 0x20080
+#define VMP_SZ 0x20080
 #define MC_SZ 0x20000
 
 void XorWithByte(uint8_t* buf, uint8_t byte, int length)
@@ -31,7 +29,6 @@ void XorWithByte(uint8_t* buf, uint8_t byte, int length)
 	}
 }
 
-
 static void usage(char *argv[])
 {
 	printf("\nvita-mcr2vmp by @dots_tb\nWith CBPS help especially: @AnalogMan151 and @teakhanirons\n");
@@ -39,7 +36,6 @@ static void usage(char *argv[])
 	printf("extracts MCR files from Sony's PSP VMP save file format.\n\n");
 	printf("Usage: %s <memorycard.mcr|SCEVMC*.VMP>\n",argv[0]);
 }
-
 
 int main(int argc, char **argv)
 {
@@ -52,7 +48,7 @@ int main(int argc, char **argv)
 	FILE *fin, *fout;
 	char mc_magic[4] = {0x4D, 0x43, 0x00, 0x00};
 	char vmp_magic[4] = {0x00, 0x50, 0x4D, 0x56};
-	char buf[4];
+	char magic_buf[4];
 	bool mcr, vmp = false;
 	fin = fopen(argv[1], "rb");
 	if (!fin) {
@@ -62,10 +58,10 @@ int main(int argc, char **argv)
 
 	// Check MAGIC
 	fseek(fin, 0, SEEK_SET);
-	fread(buf, 1, 4, fin);
-	if (memcmp(buf, mc_magic, 4) == 0) {
+	fread(magic_buf, 1, 4, fin);
+	if (memcmp(magic_buf, mc_magic, 4) == 0) {
 		mcr = true;
-	} else if (memcmp(buf, vmp_magic, 4) == 0) {
+	} else if (memcmp(magic_buf, vmp_magic, 4) == 0) {
 		vmp = true;
 	} else {
 		perror("File is not supported");
@@ -75,7 +71,7 @@ int main(int argc, char **argv)
 
 	// Passes check, strips VMP header and writes MCR
 	if (vmp) {
-		char mcbuf[0x20000];
+		char mcbuf[MC_SZ];
 		fseek(fin, MCR_OFFSET, SEEK_SET);
 		fread(mcbuf, 1, MC_SZ, fin);
 		fclose(fin);
@@ -86,27 +82,23 @@ int main(int argc, char **argv)
 			perror("Failed to open output file");
 			goto error;
 		}
-		fwrite(mcbuf, 1, 0x20000, fout);
+		fwrite(mcbuf, 1, MC_SZ, fout);
 		printf("MCR file successfully extracted.\n");
 	}
 
 	// Passes check, writes MCR with signed VMP header
 	if (mcr) {
-		fseek(fin, 0, SEEK_SET);
-		
-		size_t sz = FILE_SZ;
-		uint8_t *input = (unsigned char*) calloc (1, sz);
+		uint8_t *input = (unsigned char*) calloc (1, VMP_SZ);
 		uint32_t *input_ptr = (uint32_t*) input;
 		input_ptr[0] = PMV_MAGIC;
 		input_ptr[1] = MCR_OFFSET;
 		
 		fseek(fin, 0, SEEK_SET);
-		fread(input + MCR_OFFSET, sz, 1, fin);
+		fread(input + MCR_OFFSET, MC_SZ, 1, fin);
 		
 		struct AES_ctx aes_ctx;
 		AES_init_ctx_iv(&aes_ctx, key, iv);
 		
-
 		uint8_t salt[0x40];
 		uint8_t work_buf[0x14];
 		
@@ -120,39 +112,31 @@ int main(int argc, char **argv)
 		AES_ECB_encrypt(&aes_ctx, work_buf);
 		memcpy(salt + 0x10, work_buf, 0x10);
 
-		
 		XorWithIv(salt, iv);
 		
 		memset(work_buf, 0xFF, sizeof(work_buf));
 		memcpy(work_buf, salt_seed + 0x10, 0x4);
 		XorWithIv(salt + 0x10, work_buf);
 
-		
 		memset(salt + 0x14, 0, sizeof(salt) - 0x14);
 		XorWithByte(salt, 0x36, 0x40);
 		
 		SHA1_CTX sha1_ctx_1;	
 		SHA1Init(&sha1_ctx_1);
 	
+		SHA1Update(&sha1_ctx_1, salt, 0x40);
 
-		
-		SHA1Update( &sha1_ctx_1, salt, 0x40);
-
-		
 		memset(input + 0x20, 0, 0x14);
-		SHA1Update( &sha1_ctx_1, input, sz);
-			
-			
+		SHA1Update(&sha1_ctx_1, input, VMP_SZ);
+				
 		XorWithByte(salt, 0x6A, 0x40);
 
 		SHA1Final(work_buf, &sha1_ctx_1);
 
 		SHA1_CTX sha1_ctx_2;
 		SHA1Init(&sha1_ctx_2);
-		SHA1Update( &sha1_ctx_2, salt, 0x40);
-		SHA1Update( &sha1_ctx_2, work_buf, 0x14);
-
-
+		SHA1Update(&sha1_ctx_2, salt, 0x40);
+		SHA1Update(&sha1_ctx_2, work_buf, 0x14);
 
 		SHA1Final(input + HASH_OFFSET, &sha1_ctx_2);
 		
@@ -169,14 +153,15 @@ int main(int argc, char **argv)
 			perror("Failed to open output file");
 			goto error;
 		}
-		fwrite(input,  1, FILE_SZ, fout);
+		fwrite(input,  1, VMP_SZ, fout);
 		printf("VMP created successfully.\n");
 	}
+
 error:
 	if (fin)
 		fclose(fin);
 	if (fout)
 		fclose(fout);	
-	exit(0);
+
 	return 0;
 }
